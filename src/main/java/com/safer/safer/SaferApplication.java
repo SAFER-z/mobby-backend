1d36c67d2613e5551f2479e2cc9894473305cafd
yun8565 <yunyun8565@naver.com>
2023-09-23 22:17:43 +0900

    Initial commit

---
+++ src/main/java/com/safer/safer/SaferApplication.java
@@ -0,0 +1,13 @@
+package com.safer.safer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safer/safer/SaferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safer.safer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
