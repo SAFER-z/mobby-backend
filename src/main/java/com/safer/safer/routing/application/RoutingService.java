96578c4273b469aec603d423babdf3653c37cc2d
yun8565 <yunyun8565@naver.com>
2024-01-06 22:32:19 +0900

    feat: 통합 검색 기능 추가
    - 검색 키워드가 숫자거나 한자리인 경우 도로명주소 검색 과정에서 예외가 발생하기 때문에 해당 상황에는 도로명주소 검색을 건너뛰도록 한다.
    - 도로명주소 API 호출 시 발생할 수 있는 예외를 ControllerAdvice에 추가
    - ControllerAdvice 예외 성격에 맞게 메소드 분리
    - API 문서 업데이트

--- src/main/java/com/safer/safer/routing/application/RoutingService.java
+++ src/main/java/com/safer/safer/routing/application/RoutingService.java
@@ -2,16 +2,18 @@ package com.safer.safer.routing.application;
 
 import com.safer.safer.facility.application.FacilityService;
 import com.safer.safer.facility.dto.CoordinateRequest;
-import com.safer.safer.facility.dto.FacilitiesDistanceResponse;
 import com.safer.safer.facility.dto.FacilityDistanceResponse;
 import com.safer.safer.routing.dto.SearchResponse;
 import com.safer.safer.routing.dto.address.AddressResponse;
 import com.safer.safer.routing.infrastructure.address.AddressRequester;
+import com.safer.safer.station.application.StationService;
+import com.safer.safer.station.dto.StationDistanceResponse;
 import lombok.RequiredArgsConstructor;
-import org.locationtech.jts.geom.Point;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static com.safer.safer.batch.util.BatchConstant.NUMBER_REGEX;
 
 @Service
 @RequiredArgsConstructor
@@ -19,11 +21,19 @@ public class RoutingService {
 
     private final AddressRequester addressRequester;
     private final FacilityService facilityService;
+    private final StationService stationService;
 
     public SearchResponse searchByKeyword(String keyword, CoordinateRequest coordinate) {
-        List<AddressResponse> addressSearchResult = addressRequester.requestAddress(keyword);
-        List<FacilityDistanceResponse> facilitySearchResult = facilityService.searchFacilities(keyword, coordinate);
 
-        return SearchResponse.of(facilitySearchResult, addressSearchResult);
+        List<AddressResponse> addressSearchResult = isValidKeywordForAddress(keyword) ?
+                addressRequester.requestAddress(keyword) : List.of();
+        List<FacilityDistanceResponse> facilitySearchResult = facilityService.searchFacilities(keyword, coordinate);
+        List<StationDistanceResponse> stationSearchResult = stationService.searchStations(keyword, coordinate);
+
+        return SearchResponse.of(facilitySearchResult, stationSearchResult, addressSearchResult);
+    }
+
+    private boolean isValidKeywordForAddress(String keyword) {
+        return !keyword.matches(NUMBER_REGEX) && keyword.length() > 1;
     }
 }
